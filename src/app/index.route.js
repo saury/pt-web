ee206504c8f956f1dbb2e7ed3da654021479e66a
Jin Xu <ext.jin.xu@ef.com>
2017-01-23 10:54:01 +0800

    improve the slide animation in login series page

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -23,21 +23,25 @@
         controllerAs: 'login'
       })
       .state('login.country', {
+        // url: '/country',
         templateUrl: 'app/login/country/country.html',
         controller: 'CountryController',
         controllerAs: 'country'
       })
       .state('login.school', {
+        // url: '/school',
         templateUrl: 'app/login/school/school.html',
         controller: 'SchoolController',
         controllerAs: 'school'
       })
       .state('login.role', {
+        // url: '/role',
         templateUrl: 'app/login/role/role.html',
         controller: 'RoleController',
         controllerAs: 'role'
       })
       .state('login.user', {
+        // url: '/user',
         templateUrl: 'app/login/user/user.html',
         controller: 'UserController',
         controllerAs: 'user'
--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -7,46 +7,69 @@
 
   /** @ngInject */
   function LoginController($log, $timeout, $scope, $state, $rootScope) {
-      var vm = this,
+    var vm = this,
       queue = [
         'country',
         'school',
         'role',
         'user'
-    ];
-      // selected info, default val all undefined
-      vm.select = {
-        country: void 0,
-        school: void 0,
-        role: void 0,
-        user: void 0
-      }
-      
-      // watch the user selection and change the router
-      $scope.$on('selectionChanges', function(){
-        render();
-      });
+      ],
+      currentPostion,
+      initTimer;
 
-      $timeout(render, 0, false)
+    // selected info, default val all undefined
+    vm.select = {
+      country: void 0,
+      school: void 0,
+      role: void 0,
+      user: void 0
+    }
 
-      function render() {
-        for (var i = 0, len = queue.length; i < len; i++) {
-            if(!vm.select[queue[i]]) {
-              $state.go('login.'+ queue[i]);
-              break;
-            }
-            if(i === 3) {
-              $log.debug('go to main')
-              $rootScope.login = true;
-              $state.go('main')
-            }
+    initTimer = $timeout(function () {
+      // render for the 1st time
+      render();
+      addListener();
+    }, 0, false)
+
+    /**
+     * Render the login page according to the user's selected infos
+     */
+    function render() {
+      for (var i = 0, len = queue.length; i < len; i++) {
+        if (!vm.select[queue[i]]) {
+          vm.forward = true;
+          $state.go('login.' + queue[i]);
+          // save the position in order to go back manually
+          currentPostion = i;
+          break;
+        }
+        if (i === 3) {
+          $log.debug('go to main')
+          $rootScope.login = true;
+          $state.go('main');
         }
       }
+    }
 
-      // vm.timer = $timeout(function(){
-        // $state.transitionTo('login.country')
-      //   $log.debug('login ctrl')
-      // }, 0, false)
-    
+    function addListener() {
+      // prevent go back to path '/' directly
+      $scope.$on('$locationChangeStart', function (event, toUrl) {
+        if (!(currentPostion === 3 || currentPostion === 0)) {
+          event.preventDefault()
+          $log.debug(toUrl);
+          vm.forward = false;
+          currentPostion -= 1;
+          $state.go('login.' + queue[currentPostion]);
+        }
+      });
+      // watch the user selection and rerender the page
+      $scope.$on('selectionChanges', function () {
+        render();
+      });
+    }
+
+    $scope.$on('$destroy', function () {
+      $timeout.cancel(initTimer);
+    })
   }
 })();
